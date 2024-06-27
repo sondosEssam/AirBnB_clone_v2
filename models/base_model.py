12995dd67c7265fff1bd001b1db46f1ea3a46dcd
SondosEssam <sondosessam713@gmail.com>
2024-06-28 01:46:42 +0300

    new

--- models/base_model.py
+++ models/base_model.py
@@ -15,6 +15,13 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
+            if not kwargs['updated_at'] :
+                from models import storage
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+                
+                storage.new(self)
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
