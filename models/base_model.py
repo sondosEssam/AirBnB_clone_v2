36e27a70a98f55d7f94d3849ea2b9c88561846c7
SondosEssam <sondosessam713@gmail.com>
2024-06-30 03:58:58 +0300

    sh

--- models/base_model.py
+++ models/base_model.py
@@ -15,19 +15,22 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            if not kwargs['updated_at'] :
+            if kwargs.get('updated_at') is None:
                 from models import storage
                 self.id = str(uuid.uuid4())
                 self.created_at = datetime.now()
                 self.updated_at = datetime.now()
-                
+                for key, val in kwargs.items():
+                    if key != "updated_at" and key != "created_at":
+                        setattr(self, key, val)
                 storage.new(self)
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            else:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                            '%Y-%m-%dT%H:%M:%S.%f')
+                del kwargs['__class__']
+                self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
